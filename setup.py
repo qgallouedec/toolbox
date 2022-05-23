2e68f859b384c5c0a8a73048346981dde1503d5d
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-05-23 22:52:17 +0200

    save python func

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="toolbox",
+    packages=find_packages(),
+    url="https://github.com/qgallouedec/toolbox",
+    description="Some usefull tools",
+    long_description=open("README.md").read(),
+    install_requires=["numpy", "scipy", "rliable"],
+    extras_require={
+        "extras": ["pytest", "black", "isort"],
+    },
+)
---
+++ toolbox/__init__.py
@@ -0,0 +1,1 @@
+from toolbox.to_dat import save_iqm, save_median, save_performance_profile
