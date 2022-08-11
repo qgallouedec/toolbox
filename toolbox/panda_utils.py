e985fb21be8da5ca74f2c9fa9fdc77e593ed8612
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-08-11 08:55:52 +0200

    faster compute coverage

--- toolbox/panda_utils.py
+++ toolbox/panda_utils.py
@@ -3,18 +3,13 @@ import numpy as np
 
 def compute_coverage(observations):
     num_timesteps, num_envs = observations.shape[0], observations.shape[1]
-    cells = np.floor(observations * 10)  # NUM_TIMESTEPS  x NUM_ENV x 3
-    seen_cells = []
-    counts = np.zeros(num_timesteps)
+    cells = np.floor(observations * 10)
+    unique, cell_uids = np.unique(np.squeeze(cells), axis=0, return_inverse=True)
+    seen_uids = []
+    counts = np.zeros(num_timesteps, dtype=np.int32)
     for t in range(num_timesteps):
-        for env_idx in range(num_envs):
-            is_new = True
-            cell = cells[t, env_idx]
-            for seen_cell in seen_cells:
-                if (seen_cell == cell).all():
-                    is_new = False
-                    break
-            if is_new:
-                seen_cells.append(cell)
-        counts[t] = len(seen_cells)
+        cell_uid = cell_uids[t]
+        if not cell_uid in seen_uids:
+            seen_uids.append(cell_uid)
+        counts[t] = len(seen_uids)
     return counts
